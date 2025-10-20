241a456ddf5b4df824f8e577071d3afad075cd51
Sonny Sangha <sonny.sangha@gmail.com>
2025-10-20 21:13:36 +0400

    Add Clerk authentication middleware and integrate ClerkProvider in the layout. Update dependencies for @clerk/nextjs in package.json and pnpm-lock.yaml.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SanityLive } from "@/sanity/lib/live";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,15 +25,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
 
-        {/* Live content API */}
-        <SanityLive />
-      </body>
-    </html>
+          {/* Live content API */}
+          <SanityLive />
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
