2814f54945c97d566f9ef7720963f85a12f17bad
Sonny Sangha <sonny.sangha@gmail.com>
2025-10-20 21:44:20 +0400

    Add components.json for configuration, implement FloatingDock component with responsive design, and update globals.css for new styles. Remove old page.tsx and create new layout and page files for portfolio and Sanity studio integration. Update package.json and pnpm-lock.yaml with new dependencies including tailwind-merge and animation libraries.

---
+++ components/FloatingDock.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import React from "react";
+import { FloatingDock as FloatingDockComponent } from "@/components/ui/floating-dock";
+import {
+  IconBrandGithub,
+  IconBrandX,
+  IconExchange,
+  IconHome,
+  IconNewSection,
+  IconTerminal2,
+} from "@tabler/icons-react";
+
+export function FloatingDock() {
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Products",
+      icon: (
+        <IconTerminal2 className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Components",
+      icon: (
+        <IconNewSection className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "Aceternity UI",
+      icon: (
+        <img
+          src="https://assets.aceternity.com/logo-dark.png"
+          width={20}
+          height={20}
+          alt="Aceternity Logo"
+        />
+      ),
+      href: "#",
+    },
+    {
+      title: "Changelog",
+      icon: (
+        <IconExchange className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Twitter",
+      icon: (
+        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+  ];
+  return (
+    <div className="fixed bottom-4 md:left-1/2 md:-translate-x-1/2 right-4 md:right-auto z-50">
+      <FloatingDockComponent items={links} />
+    </div>
+  );
+}
